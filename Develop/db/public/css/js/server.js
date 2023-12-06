17b4fd0b49bb9025695745a33f05c3781abb36e1
Bjpippenger <144498503+Bjpippenger@users.noreply.github.com>
2023-12-05 18:35:55 -0800

    update

--- Develop/db/public/css/js/server.js
+++ Develop/db/public/css/js/server.js
@@ -1,36 +1,50 @@
 const express = require('express');
-const fs = require('fs');
 const path = require('path');
+const fs = require('fs');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// Set up Express to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
 // HTML Routes
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
+  res.sendFile(path.join(__dirname, '/public/index.html'));
 });
 
 app.get('/notes', (req, res) => {
-  res.sendFile(path.join(__dirname, 'notes.html'));
+  res.sendFile(path.join(__dirname, '/public/notes.html'));
 });
 
 // API Routes
 app.get('/api/notes', (req, res) => {
-  const notes = JSON.parse(fs.readFileSync('db.json', 'utf8'));
+  const notes = JSON.parse(fs.readFileSync('./db.json', 'utf8'));
   res.json(notes);
 });
 
 app.post('/api/notes', (req, res) => {
   const newNote = req.body;
-  const notes = JSON.parse(fs.readFileSync('db.json', 'utf8'));
-  newNote.id = generateUniqueId(); // You'll need to implement a function to generate a unique ID
+  const notes = JSON.parse(fs.readFileSync('./db.json', 'utf8'));
+  newNote.id = notes.length.toString();
   notes.push(newNote);
-  fs.writeFileSync('db.json', JSON.stringify(notes));
+  fs.writeFileSync('./db.json', JSON.stringify(notes));
   res.json(newNote);
+});
+
+app.delete('/api/notes/:id', (req, res) => {
+  const noteId = req.params.id;
+  const notes = JSON.parse(fs.readFileSync('./db.json', 'utf8'));
+  const noteIndex = notes.findIndex((note) => note.id === noteId);
+  if (noteIndex !== -1) {
+    notes.splice(noteIndex, 1);
+    fs.writeFileSync('./db.json', JSON.stringify(notes));
+    res.json({ message: 'Note deleted successfully' });
+  } else {
+    res.status(404).json({ error: 'Note not found' });
+  }
 });
 
 // Start the server
